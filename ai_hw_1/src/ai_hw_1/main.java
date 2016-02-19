936b0bbd3ae74e120049d5f03b8d0417990c1140
inci boduroglu <inci.boduroglu@gmail.com>
2016-02-19 11:38:31 +0200

    main eklendi
    meyin

---
+++ ai_hw_1/src/ai_hw_1/main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ai_hw_1;
+
+/**
+ *
+ * @author inciboduroglu
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
