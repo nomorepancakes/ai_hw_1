7c03fbf455d984644ffe759e9c96f676e8af03f2
inci boduroglu <inci.boduroglu@gmail.com>
2016-02-20 14:27:41 +0200

    test

--- ai_hw_1/src/Problem.java
+++ ai_hw_1/src/Problem.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ArrayList;
 //package ai_hw_1;
 
 /**
@@ -12,6 +13,10 @@ public class Problem{
     
     public LinkedList stateList;
     
+    int [][] transArray;
+    
+    boolean illegal;
+    
     public Problem(){
         s0 = new State();
         goal = new State();
@@ -21,37 +26,42 @@ public class Problem{
         stateList.add(s0);
         
         goal.setGoal();     // set goal the goal state
+        
+        illegal = false;
     }
     
     public int funct(){     // Path Finder function
         // Generate state
         // Put it in the queue randomly
         s0.printState();
-        
         tempState = s0.transition(2, 0);
         stateList.add(tempState);
         tempState.printState();
         
-        
-        tempState = tempState.transition(1, 0);
-        if(tempState == null){
-            System.out.println("naber");
+        while (!tempState.equals(goal)){
+            tempState = tempState.transition(2, 0);
+            if(!checkState(tempState)){
+                stateList.add(tempState);
+                tempState.printState();
+                
+                //tempState = new State();
+                break;
+            }
         }
-        stateList.add(tempState);
-        tempState.printState();
         
-        tempState = tempState.transition(2, 0);
-        stateList.add(0, tempState);
-        tempState.printState();
-        
-        tempState = new State();
-        tempState = (State) stateList.get(0);
-        /*
-        tempState = s0.transition(1, 0);
-        tempState.printState();
-        
-        tempState = s0.transition(0, 2);
-        tempState.printState();*/
         return 0;
+    }
+    
+    private boolean checkState(State s){
+        if(s == null){
+            System.out.println("illegal");
+            return true;
+        }
+        
+        return false;
+    }
+    
+    private void initTransArray(){
+        
     }
 }
